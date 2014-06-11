818a6674984cb1d58df39ce875c5fcee70a6feaf
Sumit Gouthaman <sumitgt007@gmail.com>
2014-06-11 16:18:01 +0530

    Added basic layout of UDP/TCP sending screen

--- app/src/main/java/com/sumitgouthaman/socsend/app/general_utils/EndPoint.java
+++ app/src/main/java/com/sumitgouthaman/socsend/app/general_utils/EndPoint.java
@@ -6,4 +6,14 @@ package com.sumitgouthaman.socsend.app.general_utils;
 public class EndPoint {
     public String ipAddress;
     public int port;
+
+    public EndPoint(String ipAddress, int port) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    public EndPoint(int port) {
+        this.ipAddress = null;
+        this.port = port;
+    }
 }
